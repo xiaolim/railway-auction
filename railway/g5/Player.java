ac9431fa815049820e6f3a4689e4f3faf83b5a61
Mehmet Sonmez <mes2311@columbia.edu>
2018-10-29 11:15:02 -0400

    updating valuations x10

--- railway/g5/Player.java
+++ railway/g5/Player.java
@@ -31,58 +31,54 @@ public class Player implements railway.sim.Player {
         List<Coordinates> geo,
         List<List<Integer>> infra,
         int[][] transit,
-        List<String> townLookup) {
+        List<String> townLookup){
 
         this.budget = budget;
 
+        Map<Integer, Integer> cityTraffic = new HashMap<Integer, Integer>();
+        for (int m=0; m<geo.size(); m++){
+          cityTraffic.put(m, 0);
+          for (int n=0; n<geo.size(); n++){
+            if( n == m ) {
+              for(int p=0; p<geo.size(); p++){
+                cityTraffic.put(m, cityTraffic.get(m)+transit[n][p]);
+              }
+            } else {
+              cityTraffic.put(m, cityTraffic.get(m)+transit[n][m]);
+            }
+          }
+        }
 
-	Map<Integer, Integer> cityTraffic = new HashMap<Integer, Integer>();
-
-	for (int m=0; m<geo.size(); m++){
-	    cityTraffic.put(m, 0);
-	    for (int n=0; n<geo.size(); n++){
-		if( n == m ) {
-		    for(int p=0; p<geo.size(); p++){
-			cityTraffic.put(m, cityTraffic.get(m)+transit[n][p]);
-		    }
-		} else {
-		    cityTraffic.put(m, cityTraffic.get(m)+transit[n][m]);
-		}
-	    }
-	}
-
-	int id = 0;
-
-	for (int i=0; i<infra.size(); ++i){
-	    for (int j=0; j<infra.get(i).size(); ++j){
-		int irails = infra.get(i).size();
-		int jrails = infra.get(infra.get(i).get(j)).size();
+        int id = 0;
+        for (int i=0; i<infra.size(); ++i){
+          for (int j=0; j<infra.get(i).size(); ++j){
+            int irails = infra.get(i).size();
+            int jrails = infra.get(infra.get(i).get(j)).size();
 
 		//		System.out.printf("i is %d, irails starting at %d\n", i, irails);
 		//		System.out.printf("j is %d, jrails starting at %d\n", j, jrails);
 
-
-		for (int k=0; k<infra.size(); ++k){
-		    if (infra.get(k).contains(i) ){
-			irails +=1;
+            for (int k=0; k<infra.size(); ++k){
+              if (infra.get(k).contains(i) ){
+                irails +=1;
 			//	System.out.printf("found a %d in line %d, irails now %d\n", i, k, irails);
-		    }
-		    if (infra.get(k).contains(infra.get(i).get(j)) ){
-			jrails +=1;
+		          }
+              if (infra.get(k).contains(infra.get(i).get(j)) ){
+                jrails +=1;
 			//	System.out.printf("found a %d in line %d, jrails now %d\n", j, k, jrails);
-		    }
-		}
-		int value = 0;
-		if (irails != 0){
-		    value += cityTraffic.get(i)/irails;
-		}
-		if (jrails != 0){
-		    value += cityTraffic.get(j)/jrails;
-		}
-		railValues.put(id, value);
-		id++;
-	    }
-	}
+		          }
+		        }
+		        int value = 0;
+            if (irails != 0){
+              value += cityTraffic.get(i)/irails;
+            }
+            if (jrails != 0){
+              value += cityTraffic.get(j)/jrails;
+            }
+            railValues.put(id, value);
+            id++;
+          }
+        }
     }
 
     public Bid getBid(List<Bid> currentBids, List<BidInfo> allBids) {
@@ -173,6 +169,16 @@ public class Player implements railway.sim.Player {
 
 
         // Increment bid on a valuable enough link
+        for (int linkId : availableLinks){
+          if(railValues.get(linkId)*10 > minAmounts.get(linkId) + 10000){
+            Bid bid = new Bid();
+            bid.id1 = linkId;
+            bid.amount = minAmounts.get(linkId) + 10000;
+            return bid;
+          }
+
+
+        }
 
 
         // Random player doesn't care about bids made by other players.
