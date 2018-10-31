e0108c93fd4d4c85df815370a4c27f0cab3b6eea
Evan Ziebart <erziebart@gmail.com>
2018-10-31 12:26:14 -0400

    profit maximization on single links

---
+++ railway/g3/G3Bid.java
@@ -0,0 +1,18 @@
+package railway.g3;
+
+// To access data classes.
+import railway.sim.utils.*;
+
+public class G3Bid extends Bid {
+	// id of first town
+	public int town_id1;
+
+	// id of second town
+	public int town_id2;
+
+	// id of third town -- if applicable
+	public int town_id3;
+
+	// minimum bid that could ever win the link
+	public double min_bid;
+}
