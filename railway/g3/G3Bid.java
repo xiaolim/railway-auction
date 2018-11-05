6f870e90b439c0b2cb6bc7677e01af9d430770e2
Shandukani Mulaudzi <shandu.mulaudzi@gmail.com>
2018-11-04 21:00:21 -0500

    Override equals method for G3Bid

--- railway/g3/G3Bid.java
+++ railway/g3/G3Bid.java
@@ -3,6 +3,8 @@ package railway.g3;
 // To access data classes.
 import railway.sim.utils.*;
 
+import java.util.Arrays;
+
 public class G3Bid extends Bid {
 	// id of first town
 	public int town_id1;
@@ -20,5 +22,26 @@ public class G3Bid extends Bid {
 		town_id1 = -1;
 		town_id2 = -1;
 		town_id3 = -1;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (o == null) {
+			return false;
+		}
+		G3Bid otherBid = (G3Bid) o;
+		if ((otherBid.town_id3 < -1 && this.town_id3 > -1) || (otherBid.town_id3 > -1 && this.town_id3 < -1)) {
+			return false;
+		}
+		int[] otherIDs = {otherBid.town_id1, otherBid.town_id2, otherBid.town_id3};
+		int[] thisIDs = {this.town_id1, this.town_id2, this.town_id3};
+		Arrays.sort(otherIDs);
+		Arrays.sort(thisIDs);
+		if (Arrays.equals(otherIDs, thisIDs)) {
+			return true;
+		}
+		return false;
+
 	}
 }
