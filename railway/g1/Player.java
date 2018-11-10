f7140b1d1490eef1a227a8ec4c2fc2ffc35696d8
Bohan Qu <qubh100@163.com>
2018-11-09 23:40:15 -0500

    merge, bug fix

--- railway/g1/Player.java
+++ railway/g1/Player.java
@@ -72,7 +72,7 @@ public class Player implements railway.sim.Player {
 		int i1;
     	int i2;
     	Pair(int i1, int i2){
-    		if (i1 < i2) {
+    		if (i1 > i2) {
     			this.i1 = i2;
     			this.i2 = i1;
     		}
@@ -653,16 +653,45 @@ public class Player implements railway.sim.Player {
     }
         
         // create heat map, TODO Wanlin
-        for (Map.Entry<Pair, List<List<Integer>>> entry : k_shortest_paths.entrySet()) {
-        	List<Double> distances = new ArrayList<Double>();
-        	for (List<Integer> path : entry.getValue()) {
-        		distances.add(player_path_distance.get(path));
-        	}
-        	for (List<Integer> path : entry.getValue()) {
-        		// TODO
-        	}
+        Map<Pair, Double> newmap = new HashMap<Pair, Double>();
+        for(int i=0;i<infra.size();i++) {
+            for(int j=0;j<infra.get(i).size();j++) {
+                newmap.put(new Pair(i,infra.get(i).get(j)),0.0);
+            }
         }
-        return convertHeatMap(heatmap);
+        for (int i=0;i<transit.length;i++) {
+            for (int j=0;j<transit[i].length;j++) { //int j=0;j<transit[i].length;j++
+                if(transit[i][j]==0) {
+                    continue;
+                }
+                List<List<Integer>> allP = k_shortest_paths.get(new Pair(i,j));
+                Map<Integer, Double> weights = new HashMap<Integer, Double>();
+                Map<Integer, Double> distances = new HashMap<Integer, Double>();
+                Double maxdistance = -1.0D;
+                for(int a=0;a<allP.size();a++) {
+                    Double temp = player_path_distance.get(allP.get(a));
+                    distances.put(a,temp);
+                    if(temp > maxdistance) {
+                        maxdistance = temp;
+                    }
+                }
+                for(Integer a:distances.keySet()) {
+                    weights.put(a,distances.get(a)/maxdistance); //scale so everything is under 1
+                }
+                Map<Integer, Double> softmaxWeights = softmaxDistance(weights);
+                for(int a=0;a<allP.size();a++) {
+                    for(int b=0;b<allP.get(a).size();b++) {
+                        if(b>0) {
+                            int t1 = allP.get(a).get(b-1); 
+                            int t2 = allP.get(a).get(b); 
+                            Pair link = new Pair(t1,t2);
+                            newmap.put(link,newmap.get(link)+distances.get(a)*softmaxWeights.get(a));
+                        }
+                    }
+                }
+            }
+        }
+        return convertHeatMap(newmap);
     }
 
 
